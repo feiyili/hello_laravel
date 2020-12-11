bd3aecbe296b47e794892bdb303ccf0c6bdbb1b5
绯翼君 <ljs_jee@163.com>
2020-12-12 01:15:44 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    <style>
+        html, body {
+            height:100%;
+        }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: Arial, Helvetica, sans-serif;
+            color:#777;
+        }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+        .title {
+            font-size: 66px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">
+                Hello Laravel! -feiyijun
+            </div>
+        </div>
+    </div>
+</body>
+</html>
